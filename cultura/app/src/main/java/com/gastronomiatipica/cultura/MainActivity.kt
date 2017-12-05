94fd65261f6c39425de12594e04d25b528f2d0fc
giltamayo <tamayo_144@hotmail.com>
2017-12-05 12:29:36 -0600

    Evento boton tuto No 10

--- cultura/app/src/main/java/com/gastronomiatipica/cultura/MainActivity.kt
+++ cultura/app/src/main/java/com/gastronomiatipica/cultura/MainActivity.kt
@@ -4,14 +4,21 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity(), TextWatcher {
+class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
+
     private var editName: EditText? = null
     private var editAge: EditText? = null
+    private var textName: TextView? = null
+    private var textAge: TextView? = null
+    private var button: Button? = null
     private var name: String? = null
-    private var age = 0
+    private var age: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +26,18 @@ class MainActivity : AppCompatActivity(), TextWatcher {
 
         editName = findViewById(R.id.editText_Name)
         editAge = findViewById(R.id.editText_Age)
+        textName = findViewById(R.id.textView_Name)
+        textAge = findViewById(R.id.textView_Age)
+        button = findViewById(R.id.button_Ejecutar)
+        button!!.setOnClickListener(this)
+        editName!!.addTextChangedListener(this)
+        editAge!!.addTextChangedListener(this)
 
-        editName?.addTextChangedListener(this)
-        editAge?.addTextChangedListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        operacion()
     }
 
     override fun afterTextChanged(p0: Editable?) {
@@ -33,7 +49,15 @@ class MainActivity : AppCompatActivity(), TextWatcher {
     }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        Toast.makeText(this,s.toString(),Toast.LENGTH_SHORT).show()
+
+        //Toast.makeText(this,s.toString(),Toast.LENGTH_SHORT).show()
+    }
+
+    private fun operacion(){
+        name = editName?.text.toString()
+        age = editAge?.text.toString()
+        if(name != "")textName?.text = name
+        if(age != "")textAge?.text = age
     }
 
 }
